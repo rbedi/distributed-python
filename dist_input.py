a0f3ad924a896e7f1c9efe0eb98c8b0f69ecf33c
Varun Ramesh <varunramesh@live.com>
2017-12-10 15:29:02 -0800

    Use controller module

--- dist_input.py
+++ dist_input.py
@@ -2,6 +2,8 @@ import sys
 import select
 import tty
 
+from controller import controller
+
 original_input = input
 
 def call_key_generator(prefix):
@@ -22,15 +24,15 @@ def dist_input(prompt):
         (ready, _, _) = select.select([sys.stdin], [], [], 0)
         if ready:
             line = sys.stdin.readline()[:-1]
-            try: client.write(call_key, line, prevExist=False)
+            try: controller.write(call_key, line, prevExist=False)
             except etcd.EtcdAlreadyExist: pass
             break
         else:
             try:
-                client.read(call_key)
+                controller.read(call_key)
                 break
             except etcd.EtcdKeyNotFound:
                 pass
-    return client.read(call_key).value
+    return controller.read(call_key).value
 
 input = dist_input
