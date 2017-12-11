72886a832c9686799d8d7f3f905541e8154ecfc8
Rishi Bedi <rbedi100@gmail.com>
2017-12-11 13:10:27 +0000

    subprocess back to

--- controller.py
+++ controller.py
@@ -16,9 +16,10 @@ advertise_port = listen_port + 1
 name = os.getenv("NODE_NAME", "infra1")
 discovery_url = os.getenv("DISCOVERY_URL")
 
-command ="etcd --name {name}  --listen-client-urls http://127.0.0.1:{listen_port} --advertise-client-urls http://127.0.0.1:{listen_port} --listen-peer-urls http://127.0.0.1:{advertise_port} --initial-advertise-peer-urls http://127.0.0.1:{advertise_port} --discovery {discovery_url}".format(name=name, listen_port=listen_port, advertise_port=advertise_port, discovery_url=discovery_url)
+command ="etcd --name {name}  --listen-client-urls http://127.0.0.1:{listen_port} --advertise-client-urls http://127.0.0.1:{listen_port} --listen-peer-urls http://127.0.0.1:{advertise_port} --initial-advertise-peer-urls http://127.0.0.1:{advertise_port} --discovery {discovery_url} &".format(name=name, listen_port=listen_port, advertise_port=advertise_port, discovery_url=discovery_url)
 
-os.system(command + " &")
+#os.system(command + " &")
+subprocess.Popen(command, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
 time.sleep(0.1)
 
