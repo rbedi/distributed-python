41b06c4853a1e0dca6e4b8a5584b86f3399020b7
Varun Ramesh <varunramesh@live.com>
2017-12-10 18:20:56 -0800

    Accept varargs

--- distributed.py
+++ distributed.py
@@ -7,18 +7,16 @@ func_ctrs = defaultdict(int)
 
 def generate_distributed(orig):
 
-    def new_func():
+    def new_func(*args, **kwargs):
         name = orig.__module__ + "." + orig.__name__
         global func_ctrs
         func_ctrs[name] += 1
         key = name + '_' + str(func_ctrs[name])
         try:
-            value = orig()
+            value = orig(*args, **kwargs)
             client.write(key, value, prevExist=False)
         except etcd.EtcdAlreadyExist:
             value = client.read(key).value
         return value
 
     return new_func
-
-
