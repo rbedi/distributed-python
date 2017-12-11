bee8909067709f1827cedc833f2e5f2a11537cdc
Varun Ramesh <varunramesh@live.com>
2017-12-11 00:47:47 -0800

    Bad clock util

--- utils/bad_clock.py
+++ utils/bad_clock.py
@@ -1,8 +1,5 @@
 import os
-import datetime
-from forbiddenfruit import curse
+import time
 
 if 'BADCLOCK' in os.environ:
-    def bad_now():
-        return datetime.datetime.utcfromtimestamp(0)
-    curse(datetime.datetime, "now", bad_now)
+    time.time = lambda: 0
