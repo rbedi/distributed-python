338d6a3b93843c6cdceddfee77d369d49c6cf3bb
Kerem Goksel <bkgoksel@gmail.com>
2017-12-11 02:37:27 -0800

    Got dynamic firing up of etcd cluster

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+import subprocess
 import etcd
 import os
 
@@ -9,5 +10,11 @@ def enable_replication():
     global replication_enabled
     replication_enabled = True
 
-port = int(os.getenv("ETCD_CLIENT_PORT", 2379))
-controller = etcd.Client(port=port)
+listen_port = int(os.getenv("ETCD_CLIENT_PORT", 2379))
+advertise_port = listen_port + 1
+name = os.getenv("ETCD_NODE_NAME", "infra1")
+discovery_url = os.getenv("ETCD_DISCOVERY_URL")
+
+subprocess.Popen("etcd --name {name} --listen-client-urls http://127.0.0.1:{listen_port} --advertise-client-urls http://127.0.0.1:{listen_port} --listen-peer-urls http://127.0.0.1:{advertise_port} --initial-advertise-peer-urls http://127.0.0.1:{advertise_port} --discovery {discovery_url} &> {name}.log".format(name=name, listen_port=listen_port, advertise_port=advertise_port, discovery_url=discovery_url), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+controller = etcd.Client(port=listen_port)
--- tests/invoke-in-three-panes.sh
+++ tests/invoke-in-three-panes.sh
@@ -1,5 +1,9 @@
+rm -rf *.etcd
+
+discovery_url=$(curl https://discovery.etcd.io/new?size=3)
+
 tmux \
-  new-session  "sh -c 'ETCD_CLIENT_PORT=2379 python3 $1; sleep 100'" \; \
-  split-window  "sh -c 'ETCD_CLIENT_PORT=22379 python3 $1; sleep 100'" \; \
-  split-window  "sh -c 'ETCD_CLIENT_PORT=32379 python3 $1; sleep 100'" \; \
-  select-layout even-horizontal
+  new-session  "sh -c 'ETCD_NODE_NAME=infra1 ETCD_DISCOVERY_URL=$discovery_url ETCD_CLIENT_PORT=2379 python3 $1; sleep 100'" \; \
+  split-window "sh -c 'ETCD_NODE_NAME=infra2 ETCD_DISCOVERY_URL=$discovery_url ETCD_CLIENT_PORT=12379 python3 $1; sleep 100'" \; \
+  split-window "sh -c 'ETCD_NODE_NAME=infra3 ETCD_DISCOVERY_URL=$discovery_url ETCD_CLIENT_PORT=22379 python3 $1; sleep 100'" \; \
+select-layout even-horizontal
