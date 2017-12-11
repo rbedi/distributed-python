4159ad09f2f3e3b0300693527a0e1bc2ef7d537e
Varun Ramesh <varunramesh@live.com>
2017-12-10 19:43:49 -0800

    Fix bug.

--- controller.py
+++ controller.py
@@ -2,8 +2,14 @@ import etcd
 import os
 
 replication_enabled = True
-def disable_replication(): use_replicated_funcs = False
-def enable_replication(): use_replicated_funcs = True
+def disable_replication():
+    global replication_enabled
+    replication_enabled = False
+def enable_replication():
+    global replication_enabled
+    replication_enabled = True
 
 port = os.getenv("ETCD_CLIENT_PORT", 2379)
 controller = etcd.Client(port=port)
+
+disable_replication()
