e0a8264ca865418a6e362e6e5c2cab2f21840502
Rishi Bedi <rbedi100@gmail.com>
2017-12-11 16:16:41 -0800

    works on gcloud

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 import subprocess
 import etcd
 import os
+import sys
 
 import time
 replication_enabled = True
@@ -13,13 +14,18 @@ def enable_replication():
 
 listen_port = int(os.getenv("CLIENT_PORT", 2379))
 advertise_port = listen_port + 1
-name = os.getenv("NODE_NAME", "infra1")
+
+name =  os.getenv("NODE_NAME", "infra1")
 discovery_url = os.getenv("DISCOVERY_URL")
+ip = os.getenv("MYIP")
 
-command ="etcd --name {name}  --listen-client-urls http://127.0.0.1:{listen_port} --advertise-client-urls http://127.0.0.1:{listen_port} --listen-peer-urls http://127.0.0.1:{advertise_port} --initial-advertise-peer-urls http://127.0.0.1:{advertise_port} --discovery {discovery_url} &".format(name=name, listen_port=listen_port, advertise_port=advertise_port, discovery_url=discovery_url)
+command = "etcd --name {name} --listen-client-urls http://127.0.0.1:2379,http://{IP1}:2379  --advertise-client-urls http://127.0.0.1:2379,http://{IP1}:2379 --listen-peer-urls http://0.0.0.0:2380 --initial-advertise-peer-urls http://{IP1}:2380,http://127.0.0.1:2380 --discovery {DISCOVERY_URL}".format(
+    name=name, IP1=ip, DISCOVERY_URL=discovery_url)
 
-#os.system(command + " &")
+# command ="etcd --name {name}  --listen-client-urls http://127.0.0.1:{listen_port} --advertise-client-urls http://127.0.0.1:{listen_port} --listen-peer-urls http://127.0.0.1:{advertise_port} --initial-advertise-peer-urls http://127.0.0.1:{advertise_port} --discovery {discovery_url} &".format(name=name, listen_port=listen_port, advertise_port=advertise_port, discovery_url=discovery_url)
+# os.system(command + " &")
 subprocess.Popen(command, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+# print(discovery_url)
 
 time.sleep(0.1)
 
--- tests/etc.sh
+++ tests/etc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 export NODE_NAME=${1}
-export CLIENT_PORT=${2}
+export IP1=${2}
 
-python3 ${SCRIPT_PATH}; sleep 10000
+SSHCMD="rm -rf *.etcd; NODE_NAME=${NODE_NAME} DISCOVERY_URL=${DISCOVERY_URL} MYIP=${IP1} python3 /home/rishi/distributed-python/tests/print_time.py"
+gcloud compute --project ppi-cloud ssh --zone us-west1-b demo-1 -- "sh -c \"${SSHCMD}\""
+sleep 10000
+
+# python3 ${SCRIPT_PATH}; sleep 10000
