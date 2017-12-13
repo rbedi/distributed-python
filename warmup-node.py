4fb629ca3fe8317de38624382f39e5b092248fe8
Rishi Bedi <rbedi100@gmail.com>
2017-12-13 00:53:37 -0800

    warmup typo

--- warmup-node.py
+++ warmup-node.py
@@ -21,4 +21,4 @@ ip = os.getenv("MYIP")
 command = "etcd --name {name} --listen-client-urls http://127.0.0.1:2379,http://{IP1}:2379  --advertise-client-urls http://127.0.0.1:2379,http://{IP1}:2379 --listen-peer-urls http://0.0.0.0:2380 --initial-advertise-peer-urls http://{IP1}:2380 --discovery {DISCOVERY_URL}".format(
             name=name, IP1=ip, DISCOVERY_URL=discovery_url)
 subprocess.Popen(command, shell=True)
-Popen.wait(30)
+subprocess.Popen.wait(30)
