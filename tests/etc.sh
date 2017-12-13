3367dc883d4ee9e48e000f094fce4213324cd1e7
Rishi Bedi <rbedi100@gmail.com>
2017-12-12 23:50:46 -0800

    fixed local runner

--- tests/etc.sh
+++ tests/etc.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export NODE_NAME=${1}
 export IP1=${2}
+export ZONE=${3}
 
-SSHCMD="rm -rf *.etcd; NODE_NAME=${NODE_NAME} DISCOVERY_URL=${DISCOVERY_URL} MYIP=${IP1} python3 /home/rishi/distributed-python/${SCRIPT_PATH}"
-gcloud compute --project ppi-cloud ssh --zone us-west1-b demo-1 -- "sh -c \"${SSHCMD}\""
+SSHCMD="rm -rf *.etcd; touch 0; NODE_NAME=${NODE_NAME} DISCOVERY_URL=${DISCOVERY_URL} MYIP=${IP1} python3 /home/rishi/distributed-python/${SCRIPT_PATH}"
+gcloud compute --project ppi-cloud ssh --ssh-key-file /Users/rishi/.ssh/nopass --zone ${ZONE} ${NODE_NAME} -- "sh -c \"${SSHCMD}\""
 sleep 10000
-
-# python3 ${SCRIPT_PATH}; sleep 10000
